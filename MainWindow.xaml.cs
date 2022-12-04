44c62877dc43755ec32f07c4596ee374f8e36092
VeganGrey <beankiller185@gmail.com>
2022-12-04 11:13:32 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+using System.Windows.Markup;
+using System.Windows.Threading;
+using System.Windows.Diagnostics;
+using LibMas;
+
+namespace Prakt13
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private DispatcherTimer timer;
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(this.Timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            timer.IsEnabled = true;
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            matrrazm.Text = $"{matr.GetLength(0)+1}x{matr.GetLength(1)+1}";
+            indx.Text = $"";
+            DateTime now = DateTime.Now;
+            time.Text = now.ToString("HH:mm:ss");
+            data.Text = now.ToString("dd.MM.yyyy");
+        }
+
+        double[] matr;
+    }
+}
---
+++ Swap.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prakt13
+{
+    internal class Swap
+    {
+        public double[,] MatrixSwap(double[,] matr)
+        {
+            double[] mas = new double[matr.GetLength(1)];
+            double min = 0;double max = 0;
+            int kolmin = 0;int kolmax = 0;
+            for(int i=0;i<matr.GetLength(0);i++)
+            {
+                for(int j=0;j<matr.GetLength(1);j++)
+                {
+                    if (matr[i,j]>max)
+                    {
+                        max = matr[i,j];
+                        kolmax = i;
+                    }
+                    if (matr[i,j] <min)
+                    {
+                        min = matr[i,j];
+                        kolmin = i;
+                    }
+                }
+            }
+            for(int i=kolmin;i<mas.Length;i++)
+            {
+                mas[i] = matr[kolmin,i];
+                matr[kolmin,i] = matr[kolmax,i];
+                matr[kolmax, i] = mas[i];
+            }
+            return matr;
+        }
+    }
+}
